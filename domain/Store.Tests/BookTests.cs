3ed521bc77d47ce7d6ec0a730399c6338ec01c65
Dmitry Mieshkov <dmytro.mieshkov.work@gmail.com>
2025-05-11 13:42:36 +0300

    Add search by ISBN and Author

---
+++ domain/Store.Tests/BookTests.cs
@@ -0,0 +1,37 @@
+namespace Store.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void IsIsbn_WithNull_ReturnFalse()
+        {
+            bool actual = Book.IsIsbn(null);
+            
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsIsbn_WithBlankString_ReturnFalse()
+        {
+            bool actual = Book.IsIsbn("       ");
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsIsbn_WithInvalidIsbn_ReturnFalse()
+        {
+            bool actual = Book.IsIsbn("ISBN 123");
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsIsbn_WithIsbn10_ReturnTrue()
+        {
+            bool actual = Book.IsIsbn("IsBN 123-456-789 0 123");
+
+            Assert.True(actual);
+        }
+    }
+}
